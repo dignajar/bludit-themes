c4cc9570e29b23567f203164544b0b6a33521257
dignajar <dignajar@gmail.com>
2015-11-22 16:56:27 -0300

    Bug fixed - Future imperfect theme

--- future-imperfect/index.php
+++ future-imperfect/index.php
@@ -96,10 +96,10 @@ MIT license
 
 	<!-- Scripts -->
 	<?php Theme::jquery() ?>
-	<script src="assets/js/skel.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
-	<script src="assets/js/main.js"></script>
+	<script src="<?php echo HTML_PATH_THEME ?>assets/js/skel.min.js"></script>
+	<script src="<?php echo HTML_PATH_THEME ?>assets/js/util.js"></script>
+	<!--[if lte IE 8]><script src="<?php echo HTML_PATH_THEME ?>assets/js/ie/respond.min.js"></script><![endif]-->
+	<script src="<?php echo HTML_PATH_THEME ?>assets/js/main.js"></script>
 
 </body>
 </html>
--- future-imperfect/php/head.php
+++ future-imperfect/php/head.php
@@ -1,19 +1,18 @@
-<base href="<?php echo HTML_PATH_THEME ?>">
 <title><?php echo $Site->title() ?></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="<?php echo $Site->description() ?>">
 
-<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
-<link rel="stylesheet" href="assets/css/main.css">
-<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
-<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+<!--[if lte IE 8]><script src="<?php echo HTML_PATH_THEME ?>assets/js/ie/html5shiv.js"></script><![endif]-->
 
-<link rel="stylesheet" href="assets/css/bludit.css">
+<link rel="stylesheet" href="<?php echo HTML_PATH_THEME ?>assets/css/main.css">
+<!--[if lte IE 9]><link rel="stylesheet" href="<?php echo HTML_PATH_THEME ?>assets/css/ie9.css" /><![endif]-->
+<!--[if lte IE 8]><link rel="stylesheet" href="<?php echo HTML_PATH_THEME ?>assets/css/ie8.css" /><![endif]-->
+<link rel="stylesheet" href="<?php echo HTML_PATH_THEME ?>assets/css/bludit.css">
 
 <?php
 
-// Plugins Site Head
+// Plugins for head
 Theme::plugins('siteHead');
 
 ?>
