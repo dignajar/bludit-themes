3ec9eaaabe98cb227e50170b5f53443574eeafc2
Dipchikov <hristodipchikov@abv.bg>
2015-11-30 13:13:35 +0200

    Update

--- Business-Casual/php/home.php
+++ Business-Casual/php/home.php
@@ -20,7 +20,7 @@
     </header>
 
     <!-- Post content -->
-   <div class="col-lg-12 ">
+   <div class="col-lg-12">
         <?php
             // Call the method with FALSE to get the first part of the post
             echo $Post->content(false)
@@ -54,7 +54,7 @@
 				}
 			?>
 			<time><?php echo $Post->date() ?></time>&nbsp;|&nbsp
-			<a><?php echo $author ?></a>
+			<span  div class="name"><?php echo $author ?> </div></span>
 		</small></h6></div>
             
 	</div>
@@ -75,4 +75,4 @@
     echo Paginator::html();
 ?>                    </ul>
                 </div>
-				
+		
