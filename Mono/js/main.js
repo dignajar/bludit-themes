07062fe3eac5834ed5c8039689ab577639c8ce0d
Alexander K <alex@hound.su>
2016-04-29 09:24:45 -0400

    Mono Theme updated to v1.0

---
+++ Mono/js/main.js
@@ -0,0 +1,8 @@
+smoothScroll.init();
+
+if (self === top) {
+   var antiClickjack = document.getElementById("antiClickjack");
+   antiClickjack.parentNode.removeChild(antiClickjack);
+} else {
+   top.location = self.location;
+}
--- Mono/php/head.php
+++ Mono/php/head.php
@@ -1,5 +1,5 @@
 <meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="<?php echo $Site->description() ?>">
 <title>
   <?php	/* You can change the separator here */
@@ -15,12 +15,10 @@
 ==============================================================
 	Base by hxii (http://paulglushak.com)
 	Github: https://github.com/hxii/Monochromato
-	Release Date: 24/04/2016
-	Version: v0.1
   Remake by HoundSU (https://hound.su)
   Github: https://github.com/HoundSU/Mono
-  Release Date: 2016/04/23
-  Version: 0.2
+  Release Date: 2016/04/29
+	Version: v1.0
 ============================================================== */
 ?>
 
@@ -30,7 +28,11 @@
 		'highlight.css',
 		'scss.php?p=main.scss'
 	));
-	Theme::javascript('highlight.pack.js');
+  Theme::javascript(array(
+    'highlight.pack.js',
+    'smooth-scroll.min.js',
+    'main.min.js'
+  ));
 	Theme::favicon('favicon.png');
 	if( $Url->whereAmI()=='post' ) { Theme::keywords( $Post->tags() ); }
 	elseif( $Url->whereAmI()=='page' ) { Theme::keywords( $Page->tags() ); }
@@ -38,4 +40,7 @@
 <script>hljs.initHighlightingOnLoad();</script>
 <!-- Custom Fonts -->
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700,700italic,800&subset=latin,cyrillic' rel='stylesheet' type='text/css' rel="stylesheet" type="text/css">
+<link href='//maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css' rel='stylesheet' type='text/css' rel="stylesheet" type="text/css">
+<!-- Custom Javascript -->
+<script src="//code.jquery.com/jquery-2.2.3.min.js" integrity="sha256-a23g1Nt4dtEYOj7bR+vTu7+T8VP13humZFBJNIYoEJo=" crossorigin="anonymous"></script>
 <?php Theme::plugins('siteHead') ?>
