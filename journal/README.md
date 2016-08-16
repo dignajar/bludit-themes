9ce630776a90549da5f13a943aad09f41a495fc3
Anaggh Sreenath <anagghscm@gmail.com>
2016-08-16 16:11:07 +0530

    Updates
    * Improve Semantics
    * Validate HTML, CSS
    * Update Siimple CSS
    * Add Syntax Highlighting (highlight.js)
    * Add Smooth Scroll (Anchors will not jump abruptly)
    * data-scroll attribute added to Go Top! link and nav button
    * Update version in metadata
    * Add extra 20px padding after posts/pages

---
+++ journal/README.md
@@ -0,0 +1,5 @@
+#Journal
+
+Journal is a fast and minimalistic theme for [Bludit CMS](https://www.bludit.com/) based on the [Siimple CSS framework](https://github.com/siimple/siimple/).
+
+Demo: [Click Here!](https://anaggh.net/blog/)
--- journal/index.php
+++ journal/index.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <!-- Include HTML meta tags -->
 <?php include(THEME_DIR_PHP.'head.php') ?>
 </head>
+
 <body>
 	<div class="grid grid-fluid bodyLayout">
 		<!-- Header -->
-		<header id="header">
-			<!-- Intro -->
-			<section id="intro">
-				<header>
-					<h1><a href="<?php echo $Site->url() ?>"><?php echo $Site->title() ?></a> <span class="slogan"><?php echo $Site->slogan() ?></span>
-					<a href="#footer" class="btn-outline nav-link">Pages &amp; Tags</a></h1>
-				</header>
-			</section>
+		<header id="intro">
+			<span class="nav">
+				<a href="<?php echo $Site->url() ?>"><?php echo $Site->title() ?></a>
+				<span class="slogan"><?php echo $Site->slogan() ?></span>
+				<a data-scroll href="#footer" class="btn-outline nav-link">Pages &amp; Tags</a>
+			</span>
+		</header>
 
-		<!-- Content -->
-		<div id="content" class="grid"> <!-- content id is currently unused -->
+		<!-- Main Content -->
+		<main class="grid">
 			<?php
 			    if( ($Url->whereAmI()=='home') || ($Url->whereAmI()=='tag') || ($Url->whereAmI()=='blog') )
 			    {
@@ -32,16 +32,15 @@
 			        include(THEME_DIR_PHP.'page.php');
 			    }
 			?>
-		</div>
+		</main>
 
 		<!-- Footer  -->
-		<section id="footer">
+		<footer id="footer">
 			<?php include(THEME_DIR_PHP.'footer.php') ?>
-		</section>
-
+		</footer>
 	</div>
 
-	<!-- Scripts! Add Javascript files here if needed. -->
+	<!-- Scripts! Add Javascript files here if needed.(Low priority) -->
 
 	<!-- Plugins Site Body End -->
 	<?php Theme::plugins('siteBodyEnd') ?>
